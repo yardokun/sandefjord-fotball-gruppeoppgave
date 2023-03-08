bc1fe934e17f8670d5534c309abf85b6cb5c74d7
SolgunTT <solgun.tveit@gokstadakademiet.nogit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.name ditt>
2023-03-08 11:15:16 +0100

    slettet innhold i script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const hamburgerBtn = document.querySelector('#hamburgerBtn');
-const dropdown = document.querySelector('#dropdownMenu');
-
-
-hamburgerBtn.addEventListener('click', function() {
-  dropdown.classList.toggle('dropdown');
-  dropdown.classList.toggle('hide');
-});
